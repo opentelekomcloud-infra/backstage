4c9515c2c960602447d795c706fac152d11c0045
Artem Goncharov <artem.goncharov@gmail.com>
2023-07-03 09:30:34 +0200

    Add node-fetch for node-16
    Since it is for the moment not possible to switch to -next release (due
    to dependencies in additional plugins we pull) we need to explicitly add
    node-fetch.

--- plugins/scaffolder-backend-module-otc/src/actions/giteaPullRequest.ts
+++ plugins/scaffolder-backend-module-otc/src/actions/giteaPullRequest.ts
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import fetch from 'node-fetch';
+
 import { NotFoundError } from '@backstage/errors';
 import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
 import { ScmIntegrationRegistry } from '@backstage/integration';
