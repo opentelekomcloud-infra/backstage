96e4ec7409400c6f20349334228037e5e538e407
Vladimir Vshivkov <32225815+vladimirvshivkov@users.noreply.github.com>
2024-03-27 11:53:21 +0100

    Upgrade backstage and fix build (#38)
    Upgrade backstage and fix build
    
    Reviewed-by: Tino Schr
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -3,24 +3,24 @@ import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backen
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
-import { KeycloakOrgEntityProvider } from '@janus-idp/backstage-plugin-keycloak-backend';
+// import { KeycloakOrgEntityProvider } from '@janus-idp/backstage-plugin-keycloak-backend';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
 
-  builder.addEntityProvider(
-    KeycloakOrgEntityProvider.fromConfig(env.config, {
-      id: 'development',
-      logger: env.logger,
-      schedule: env.scheduler.createScheduledTaskRunner({
-        frequency: { minutes: 2 },
-        timeout: { minutes: 50 },
-        initialDelay: { seconds: 15 },
-      }),
-    }),
-  );
+  // builder.addEntityProvider(
+  //   KeycloakOrgEntityProvider.fromConfig(env.config, {
+  //     id: 'development',
+  //     logger: env.logger,
+  //     schedule: env.scheduler.createScheduledTaskRunner({
+  //       frequency: { minutes: 2 },
+  //       timeout: { minutes: 50 },
+  //       initialDelay: { seconds: 15 },
+  //     }),
+  //   }),
+  // );
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   builder.setProcessingIntervalSeconds(10);
   const { processingEngine, router } = await builder.build();
